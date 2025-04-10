7ab7bef50cf8f99c59faad639b7411280af897c2
maciekglowka <maciek.glowka@gmail.com>
2025-04-10 15:54:15 +0200

    unique indices

---
+++ crates/db/migration/src/m20250410_125110_add_unique_indices.rs
@@ -0,0 +1,44 @@
+use sea_orm_migration::{prelude::*, schema::*};
+
+use crate::iden::{CrateIndexIden, OwnerIden};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        let owner_index = Index::create()
+            .name("idx-owner")
+            .table(OwnerIden::Table)
+            .col(OwnerIden::CrateFk)
+            .col(OwnerIden::UserFk)
+            .unique()
+            .to_owned();
+
+        manager.create_index(owner_index).await?;
+
+        let crate_index = Index::create()
+            .name("idx-crate-index")
+            .table(CrateIndexIden::Table)
+            .col(CrateIndexIden::CrateFk)
+            .col(CrateIndexIden::Vers)
+            .unique()
+            .to_owned();
+        manager.create_index(crate_index).await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        let owner_index = Index::drop()
+            .name("idx-owner")
+            .table(OwnerIden::Table)
+            .to_owned();
+        manager.drop_index(owner_index).await?;
+
+        let crate_index = Index::drop()
+            .name("idx-crate-index")
+            .table(CrateIndexIden::Table)
+            .to_owned();
+        manager.drop_index(crate_index).await
+    }
+}
